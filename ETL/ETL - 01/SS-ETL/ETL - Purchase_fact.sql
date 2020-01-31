5eca10939fe26efd1930dce1fbad8db2f174ca93
Nithikorn-Lert <52127771+Nithikorn-Lert@users.noreply.github.com>
2020-01-31 18:17:03 +0700

    Update ETL - Purchase_fact.sql

--- ETL/ETL - 01/SS-ETL/ETL - Purchase_fact.sql
+++ ETL/ETL - 01/SS-ETL/ETL - Purchase_fact.sql
@@ -3,7 +3,7 @@ SELECT Purchases.CustID, Purchases.Date, Item_sold.ItemID, Item_sold.Qty AS Unit
 FROM     Item_sold INNER JOIN
                   Items ON Item_sold.ItemID = Items.ItemID INNER JOIN
                   Purchases ON Item_sold.TransID = Purchases.TransID INNER JOIN
-                  AllElec_star.dbo.Customer_Dim ON Purchases.CustID = AllElec_star.dbo.Customer_Dim.CustomerKey INNER JOIN
+                  AllElec_star.dbo.Customer_Dim ON Purchases.CustID = AllElec_star.dbo.Customer_Dim.CustomerID INNER JOIN
                   AllElec_star.dbo.Product_Dim ON Item_sold.ItemID = AllElec_star.dbo.Product_Dim.ProductID INNER JOIN
                   AllElec_star.dbo.Date_Dim ON Purchases.Date = AllElec_star.dbo.Date_Dim.TheDate
 
